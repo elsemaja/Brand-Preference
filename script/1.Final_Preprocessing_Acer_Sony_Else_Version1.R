54fae0df9c97ac27637e2054d8ff7adcb03b3d6c
Else Frijling <else@MacBook-Pro-van-Else.local>
2019-02-08 14:28:38 +0100

    Included working directory

--- script/1.Final_Preprocessing_Acer_Sony_Else_Version1.R
+++ script/1.Final_Preprocessing_Acer_Sony_Else_Version1.R
@@ -3,6 +3,9 @@
 # SCRIPT FUNCTION: Libraries, dataframe and engineering
 # VERSION: 2
 
+# Set working directory
+setwd("/Users/else/Google Drive/BSSA - data analytics/Module 2/Task 2 - classification brand preference")
+
 #install packages and libraries
 library(caret)
 library(readr)
@@ -10,7 +13,7 @@ library(C50)
 library(ggplot2)
 
 #import dataset CompleteRespones.csv
-CompleteResponses <- read_csv("CompleteResponses.csv")
+CompleteResponses <- read_csv("data/CompleteResponses.csv")
 
 #preprocess brand into factor
 CompleteResponses$brand <- as.factor(CompleteResponses$brand)
